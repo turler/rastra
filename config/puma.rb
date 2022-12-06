78553bf51aaceb5c7736ab9eff2fc90de2354b56
Wiink <minhdaochi504@gmail.com>
2022-12-06 22:27:03 +0700

    tempotary not use web concurrency

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,7 @@
 # and maximum; this matches the default thread size of Active Record.
 #
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
